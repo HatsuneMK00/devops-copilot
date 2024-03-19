4607301203561046e82c521376baea965f6d6a84
makise <10175101106@stu.ecnu.edu.cn>
2024-03-20 00:29:15 +0800

    feat: v0.0.1 first runnable

--- src/chatbot.py
+++ src/chatbot.py
@@ -2,14 +2,58 @@
 # created by makise, 2024/3/18
 
 import streamlit as st
+import os
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+def embed_knowledge_pdf(doc, embedding_fn, vector_store):
+    loader = PyPDFLoader("../knowledge/{}".format(doc))
+    pages = loader.load_and_split()
+    file_index = FAISS.from_documents(pages, embedding_fn)
+
+    if os.path.exists(vector_store):
+        local_index = FAISS.load_local(vector_store, embedding_fn)
+        local_index.merge_from(file_index)
+        print("Merged local index with new file {}".format(doc))
+        local_index.save_local(vector_store)
+        print("Updated index saved")
+    else:
+        file_index.save_local(vector_store)
+        print("New index created and saved")
+
+
+def get_retriever(vector_store, embedding_fn):
+    vector = FAISS.load_local(vector_store, embedding_fn, allow_dangerous_deserialization=True)
+    retriever = vector.as_retriever()
+    return retriever
+
+
+def generate_response(query, llm, retriever):
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    response = retrieval_chain.invoke({"input": query})
+    print(response["answer"])
+    return response["answer"]
 
 
 def main():
     with st.sidebar:
-        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-        "ℹ️ You can get your API key from the OpenAI dashboard."
+        st.selectbox("Select Function", ["Chatbot", "More on the way"], index=0)
 
-    st.title("💬 Chatbot")
+    st.title("💬 DevOps Copilot")
     st.caption("🚀 A DevOps copilot backed by OpenAI's GPT-3.5 and RAG.")
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
@@ -31,4 +75,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    generate_response(
+        "What is book title of the kubernetes knowledge you are referring to?",
+        ChatOpenAI(),
+        get_retriever("faiss", OpenAIEmbeddings()))
